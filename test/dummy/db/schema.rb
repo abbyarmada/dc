7d2d05cb7a8d1e0ec2b0f10a75eaf43f4bf7ddb3
Leonid Medovyy <lmedovyy@gmail.com>
2017-10-03 10:33:24 -0700

    removed dummy model

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003172754) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "blogs", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
--- test/dummy/spec/factories/blogs.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :blog do
-    name "MyString"
-    description "MyText"
-  end
-end
