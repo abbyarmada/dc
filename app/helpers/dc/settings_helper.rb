11a6fe2c3c53bace7750fa6f5ba0d5ba334f66f0
Leonid Medovyy <lmedovyy@gmail.com>
2017-10-04 16:49:12 -0700

    added settings helper

---
+++ app/helpers/dc/settings_helper.rb
@@ -0,0 +1,22 @@
+module DC
+  # Responsible for helping with the settings object
+  module SettingsHelper
+    def settings(path, options={})
+      # split the path into an array
+      path_array = path.split('.')
+      value = nil
+
+      path_array.each_with_index do |fragment, index|
+        if index.eql? 0
+          value = Settings[fragment]
+        else
+          value = value[fragment]
+        end
+        raise RuntimeError.new("fragment `#{fragment}` does not exist within `#{path}`") if value.nil? && options[:fatal_exception]
+        return nil if value.nil?
+      end
+
+      value
+    end
+  end
+end
