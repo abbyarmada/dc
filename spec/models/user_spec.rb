79c3d434285e7d848e1cab1fce0c7fdba80bb107
Leonid Medovyy <lmedovyy@gmail.com>
2017-10-08 10:41:53 -0700

    setting up default user role

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+include DC::SettingsHelper
 
 describe User, type: :model do
   it 'should have a valid factory' do
     expect(FactoryGirl.create(:user)).to be_valid
+  end
+
+  it 'should have set a valid default role' do
+    FactoryGirl.create(:user)
+    last_user = User.last
+    default_role = settings('defaults.permissions.new_user.role', fatal_exception: true).to_sym
+    expect(last_user.has_role?(default_role)).to eq true
   end
 end
--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
+  include DC::SettingsHelper
+
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    default_role = settings('defaults.permissions.new_user.role', fatal_exception: true).to_sym
+    self.add_role(default_role) if self.roles.blank?
+  end
 end
