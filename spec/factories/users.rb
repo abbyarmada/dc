1ae459cfac5a30cae827338f24b2fbae0b6ace69
Leonid Medovyy <lmedovyy@gmail.com>
2017-10-08 09:52:29 -0700

    adding rolify

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+
+    factory :admin do
+      after(:create) { |user| user.add_role(:admin) }
+    end
   end
 end
